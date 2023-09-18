41258ed5fb77cdfd592909f14d486c82a596d045
Abizer Lokhandwala <abizer@abizer.me>
2023-09-18 00:13:39 -0700

    add moreexpansions

--- plugins/expansions.py
+++ plugins/expansions.py
@@ -19,3 +19,19 @@ def chatgpt(dispatcher, payload: Payload) -> Action:
 @register(r"^c(?:al)? ?(?P<idx>\d)?$")
 def cal(dispatcher, payload: Payload, idx: int = 0) -> Action:
     return Redirect(f"https://calendar.google.com/calendar/u/{idx}/r")
+
+@register(r"^tw (?P<user>.*)")
+def twitter(dispatcher, payload: Payload, user: str) -> Action:
+    return Redirect(f"https://twitter.com/{user}")
+
+@register(r"tws (?P<query>.*)")
+def twitter_search(dispatcher, payload: Payload, query: str) -> Action:
+    return Redirect(f"https://twitter.com/search?q={query}")
+    
+@register(r"^gh (?P<user>.*)")
+def github(dispatcher, payload: Payload, user: str) -> Action:
+    return Redirect(f"https://github.com/{user}")
+
+@register(r"^ghm (?P<repo>.*)")
+def github_me(dispatcher, payload: Payload, repo: str) -> Action:
+    return Redirect(f"https://github.com/abizer/{repo}")
